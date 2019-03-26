c67bc68ba2c4a3f6d71e9a579b218d174b557d6f
yyjplus <yyjplus@163.com>
2019-03-26 16:14:23 +0800

    add mobile page

--- assets/js/getUrl.js
+++ assets/js/getUrl.js
@@ -3,6 +3,6 @@
   $.getUrlParam = function (name) {
    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
    var r = window.location.search.substr(1).match(reg);
-   if (r != null) return decodeURI(r[2]); return "天气";
+   if (r != null) return decodeURI(r[2]); return "JY罗杰";
   }
  })(jQuery);
