697b71e1c6824bf169dd1f9e2b5672e58bd702e4
yyjplus <yyjplus@163.com>
2019-03-27 15:01:59 +0800

    CIA/m.html works now

--- assets/js/getUrl.js
+++ assets/js/getUrl.js
@@ -3,6 +3,6 @@
   $.getUrlParam = function (name) {
    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
    var r = window.location.search.substr(1).match(reg);
-   if (r != null) return decodeURI(r[2]); return "JY罗杰";
+   if (r != null) return decodeURI(r[2]); return "上证指数";
   }
  })(jQuery);
